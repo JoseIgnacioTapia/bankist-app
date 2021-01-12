186570113cd1e43199f065674e5a341e6d8cded0
JoseIgnacioTapia <josignaciot@gmail.com>
2021-01-12 15:15:01 -0300

    Sorting Ascending the movements

--- script.js
+++ script.js
@@ -62,10 +62,12 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 // Display movements on the page
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort=false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit': 'withdrawal';
     
     const html = `
@@ -207,6 +209,14 @@ btnClose.addEventListener('click', function (e) {
 
   inputCloseUsername.value = inputClosePin.value = '';
 
+});
+
+// Sorting ascending the movements
+let sorted = false; 
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
 
 /////////////////////////////////////////////////
